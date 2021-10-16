4be25f831cc770908868d930153aae9cdb8add7e
tbellenger <tbellenger@gmail.com>
2021-10-16 13:34:06 -0700

    show and hide the city weather section

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,24 +2,35 @@
 let inpCitySearchEl = $('#city-search-name');
 let btnCitySearchEl = $('#city-search-btn');
 let divCitySearchHistoryEl = $('#search-history');
+let divWeatherResultsEl = $('#weather-results');
 
 // attach events
 let btnCitySearchClick = function() {
-    let cityName = inpCitySearchEl.val();
+    let cityName = inpCitySearchEl.val().trim();
+    inpCitySearchEl.val('');
     console.log('Search on city: ' + cityName);
     if (cityName !== '') {
+        //updateSearchHistory(cityName);
         getCityWeather(cityName);
+        divWeatherResultsEl.show();
     }
 }
 
-btnCitySearchEl.on('click', btnCitySearchClick);
+let btnCitySearchHistoryClick = function() {
+    inpCitySearchEl.val($(this).data('city'));
+    btnCitySearchClick();
+}
 
-let getFiveDayForecast = function(id) {
-    let response = fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + id + '&appid=4828b496af91abffe8c14b98e9eb5a2c');
+inpCitySearchEl.keypress(function(e) { if (e.which == 13) { btnCitySearchClick(); }});
+btnCitySearchEl.on('click', btnCitySearchClick);
+divCitySearchHistoryEl.on('click', '.btn', btnCitySearchHistoryClick);
+
+let getFiveDayForecast = async function(id) {
+    let response = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + id + '&appid=4828b496af91abffe8c14b98e9eb5a2c');
 
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
-        let json = response.json();
+        let json = await response.json();
         console.log(json);
     } else {
         alert("HTTP-Error: " + response.status);
@@ -27,7 +38,6 @@ let getFiveDayForecast = function(id) {
 }
 
 let getCityWeather = async function(cityName) {
-    console.log('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=4828b496af91abffe8c14b98e9eb5a2c');
     let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=4828b496af91abffe8c14b98e9eb5a2c');
 
     if (response.ok) { // if HTTP-status is 200-299
@@ -35,7 +45,7 @@ let getCityWeather = async function(cityName) {
         let json = await response.json();
         console.log(json);
 
-        console.log(json.id);
+        getFiveDayForecast(json.id);
         // update the search history with city
         // display the city current weather
         // extract the id for the city
